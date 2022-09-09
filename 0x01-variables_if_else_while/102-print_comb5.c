0f6388d48122b2a818415b3cf51200563a9fc548
Kid-jnr <normansinto3@gmail.com>
2022-09-09 15:38:18 -0700

    Print combo5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - a simple program that outputs unordered
+ * combinations of two digit integers without duplicates
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int ast;
+	int a;
+	int bfirst;
+	int b;
+
+	for (ast = 48; ast < 58; ast++)
+	{
+		for (a = 48; a < 58; a++)
+		{
+			b = a + 1;
+			bfirst = bfirst;
+			for (; bfirst < 58; bfirst++)
+			{
+				for (; b < 58; b++)
+				{
+					putchar(ast);
+					putchar(a);
+					putchar(' ');
+					putchar(bfirst);
+					putchar(b);
+					if (ast != 57 || ast != 57 || a != 56 || b != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				b = 48;
+:w
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
