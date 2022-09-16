a68a9b73f5b7a93c1974df1d3ffd543e3c195a0c
Kid-jnr <normansinto3@gmail.com>
2022-09-16 07:18:33 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * if input is a digit
+ * 
+ * Return 1 if it is a digit, 0 if it is not digit
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
