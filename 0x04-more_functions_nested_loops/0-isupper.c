97a1352ff1f9fa160de849aa11febbeb6653df80
Kid-jnr <normansinto3@gmail.com>
2022-09-16 04:11:39 -0700

    isupper in c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * _isupper - return if int c is uppercase
+ * Return: 1 if c is uppercase, 0 if it is not
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
